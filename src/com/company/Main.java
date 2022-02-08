7c193f25370f4022a65b0ebb01b77e0992e36674
Skajden <k1.greniuk@gmail.com>
2022-02-08 18:37:30 +0100

    Początek

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Jesteś Handlarzem");
+        Main game;
+        game = new Main();
+        game.handlarz();
+
+
+    }
+
+    public void handlarz(){
+
+        int handlarzGotowka;
+        handlarzGotowka = 5000;
+
+        String handlarzSamochod;
+        handlarzSamochod = "Ford";
+
+        System.out.println("handlarz posiada " + handlarzGotowka + "$ " + "oraz ma samochod: " + handlarzSamochod);
+
+
     }
 }
