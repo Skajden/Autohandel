61d0f73e69564eeaf47de63dc11f1ec0a362fd4b
Skajden <k1.greniuk@gmail.com>
2022-02-09 22:25:34 +0100

    Początek RPG

---
+++ src/com/company/Car.java
@@ -0,0 +1,23 @@
+//package com.company;
+//
+//public class Car {
+//        public Double wartosc;
+//        public String marka;
+//        public Double przebieg;
+//        public String kolor;
+//        public String segment;
+//
+//        public Car(int wartosc, String marka, int przebieg, String kolor, String segment) {
+//
+//        }
+//
+//        Car ford;
+//
+//    {
+//        ford = new Car(12000, "Mondeo", 25000, "czerwony", "premium");
+//    }
+//
+//
+//
+//
+//}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,47 +3,135 @@ package com.company;
 import java.util.Scanner;
 public class Main {
 
+    Scanner MyScanner = new Scanner(System.in);
+    Scanner enterMyScanner = new Scanner(System.in);
+
+    String graczImie;
+    String graczKlasa;
+    int graczGotowka;
+    int graczHP;
+    int choice;
+
+
+
+
     public static void main(String[] args) {
 
         Main game;
         game = new Main();
-        game.handlarz();
-        game.Salon();
+        game.gracz();
+        game.poczatek();
 
 
     }
 
-    public void handlarz(){
 
-        int handlarzGotowka;
-        handlarzGotowka = 5000;
 
-        String handlarzSamochod;
-        handlarzSamochod = "Ford";
+    public void gracz(){
 
-        System.out.println("handlarz posiada " + handlarzGotowka + "$ " + "oraz masz samochod: " + handlarzSamochod);
 
-        String HandlarzImie;
 
-        Scanner MyScanner;
+        graczGotowka = 9000;
+        graczHP = 100;
 
-        MyScanner = new Scanner(System.in);
+        System.out.println("Gracz posiada " + graczGotowka + "$ " + "oraz jego hp wynosi: " + graczHP);
+
+
 
         System.out.println("Podaj Imie:");
 
-        HandlarzImie = MyScanner.nextLine();
+        graczImie = MyScanner.nextLine();
 
-        System.out.println("Twoje imię to: " + HandlarzImie);
+
+
+        System.out.println("Podaj Klase:");
+        System.out.println("1: Wojownik");
+        System.out.println("2: Złodziej");
+
+        choice = MyScanner.nextInt();
+        graczKlasa = MyScanner.nextLine();
+
+        if(choice == 1){
+            graczKlasa = "Wojownik";
+        }
+        else if(choice == 2) {
+            graczKlasa = "Złodziej";
+        }
+
+        else {
+            System.out.println("Podałeś złą klasę postaci ");
+            System.out.println("Wybierz jeszcze raz");
+            gracz();
+
+
+        }
+
+        System.out.println("Twoje imię to: " + graczImie);
+        System.out.println("Twoje Klasa to: " + graczKlasa);
+
+        enterMyScanner.nextLine();
+
+
 
 
     }
 
-    public void Salon(){
+    public void poczatek(){
+
+
+
+
         System.out.println("----------------------");
         System.out.println("\n");
-        System.out.println("Jesteś w salonie");
-        System.out.println("");
-        System.out.println("Wybierz samochód do kupna:");
+        System.out.println("Obudziłeś się na wyspiek");
+        System.out.println(" ");
+        System.out.println("1: Przeszukaj skrzynkę :");
+        System.out.println("2: idź drogrą do najbliższego miasta :");
+        System.out.println("3: Zobacz statystyki :");
+
+
+
+        choice = MyScanner.nextInt();
+
+        if(choice==1) {
+            System.out.println("W drodze do skrzynki zatakował cie potwór");
+            if (graczKlasa.equals("Wojownik")) {
+                System.out.println("zabiłeś potwora");
+                enterMyScanner.nextLine();
+                poczatek();
+            }
+            if (graczKlasa.equals("Złodziej")) {
+
+            graczHP = graczHP - 2;
+            System.out.println("Potwór wygrał walkę i musiałeś uciec");
+            System.out.println("twoja HP wynosi:" + graczHP);
+            enterMyScanner.nextLine();
+            poczatek();
+        }
+
+        }
+        else if(choice==2){
+            System.out.println("Rozpoczołeś swoją podróż");
+            enterMyScanner.nextLine();
+            poczatek();
+
+
+        }
+
+        else if(choice==3){
+            System.out.println("Imie: " + graczImie);
+            System.out.println("Klasa: " + graczKlasa);
+            System.out.println("HP: " + graczHP);
+            System.out.println("Pieniądze: " + graczGotowka +"$");
+            enterMyScanner.nextLine();
+            poczatek();
+
+        }
+
+        else {
+            poczatek();
+
+        }
 
     }
 }
