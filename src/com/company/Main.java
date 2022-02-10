aa215d2499576380e94200d97f37c1b60b301d3f
Skajden <k1.greniuk@gmail.com>
2022-02-10 18:53:56 +0100

    Zamek

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,9 +14,11 @@ public class Main {
 
     String graczImie;
     String graczKlasa;
+    String graczBron;
     int graczGotowka;
     int graczHP;
     int choice;
+
 
 
 
@@ -28,7 +30,9 @@ public class Main {
         game.gracz();
         game.poczatek();
         game.miasto();
-
+        game.tawerna();
+        game.gildia();
+        game.opuszczonyZanek();
 
 
     }
@@ -41,8 +45,9 @@ public class Main {
 
         graczGotowka = 9000;
         graczHP = 100;
+        graczBron ="nóż";
 
-        System.out.println("Gracz posiada " + graczGotowka + "$ " + "oraz jego hp wynosi: " + graczHP);
+        System.out.println("Gracz posiada " + graczGotowka + "$ " + "jego hp wynosi: " + graczHP + "oraz posiada broń " + graczBron);
 
 
 
@@ -51,10 +56,11 @@ public class Main {
         graczImie = MyScanner.nextLine();
 
 
-
+        System.out.println("\n----------------------\n");
         System.out.println("Podaj Klase:");
         System.out.println("1: Wojownik");
         System.out.println("2: Złodziej");
+        System.out.println("\n----------------------\n");
 
         choice = MyScanner.nextInt();
         graczKlasa = MyScanner.nextLine();
@@ -73,10 +79,10 @@ public class Main {
 
 
         }
-
+        System.out.println("\n----------------------\n");
         System.out.println("Twoje imię to: " + graczImie);
         System.out.println("Twoje Klasa to: " + graczKlasa);
-        System.out.println("\n");
+        System.out.println("\n----------------------\n");
         getEnterMyScanner().nextLine();
 
 
@@ -89,23 +95,24 @@ public class Main {
 
 
 
-        System.out.println("----------------------");
-        System.out.println("\n");
+        System.out.println("\n----------------------\n");
         System.out.println("Obudziłeś się na wyspiek");
         System.out.println(" ");
         System.out.println("1: Przeszukaj skrzynkę :");
         System.out.println("2: idź drogrą do najbliższego miasta :");
         System.out.println("3: Zobacz statystyki :");
+        System.out.println("\n----------------------\n");
 
 
 
         choice = MyScanner.nextInt();
 
         if(choice==1) {
+            System.out.println("\n----------------------\n");
             System.out.println("W drodze do skrzynki zatakował cie potwór");
             if (graczKlasa.equals("Wojownik")) {
                 System.out.println("zabiłeś potwora");
-                System.out.println("\n");
+                System.out.println("\n----------------------\n");
                 getEnterMyScanner().nextLine();
                 poczatek();
             }
@@ -115,7 +122,7 @@ public class Main {
             System.out.println("Potwór  zadał ci 2 punkty obrazen");
             System.out.println("Przegrałeś walkę i musiałeś uciec");
             System.out.println("twoja HP wynosi:" + graczHP);
-            System.out.println("\n");
+            System.out.println("\n----------------------\n");
             getEnterMyScanner().nextLine();
             poczatek();
         }
@@ -124,17 +131,20 @@ public class Main {
         else if(choice==2){
             System.out.println("Rozpoczołeś swoją podróż");
             System.out.println("\n");
+            miasto();
             getEnterMyScanner().nextLine();
 
 
         }
 
         else if(choice==3){
+            System.out.println("\n----------------------\n");
             System.out.println("Imie: " + graczImie);
             System.out.println("Klasa: " + graczKlasa);
             System.out.println("HP: " + graczHP);
             System.out.println("Pieniądze: " + graczGotowka +"$");
-            System.out.println("\n");
+            System.out.println("Broń: " + graczBron);
+            System.out.println("\n----------------------\n");
             getEnterMyScanner().nextLine();
             poczatek();
 
@@ -148,14 +158,153 @@ public class Main {
     }
 
     public void miasto(){
-        System.out.println("----------------------");
-        System.out.println("\n");
+        System.out.println("\n----------------------\n");
         System.out.println("Jesteś w mięśćie");
         System.out.println(" ");
-        System.out.println("1:Tawerny:");
+        System.out.println("1:Tawerna:");
         System.out.println("2:Handlarza:");
-        System.out.println("3:Gildi:");
+        System.out.println("3:Gildia:");
+        System.out.println("4:Wróć na początek:");
+        System.out.println("\n----------------------\n");
+
+        choice = MyScanner.nextInt();
+
+        if(choice==1) {
+           tawerna();
+           getEnterMyScanner().nextLine();
+            }
+
+        if(choice==2) {
+            tawerna();
+            getEnterMyScanner().nextLine();
+        }
+
+        if(choice==3) {
+            gildia();
+            getEnterMyScanner().nextLine();
+        }
+
+        if(choice==4) {
+            System.out.println("\n----------------------\n");
+            System.out.println("3:Wróciełeś się na początek:");
+            System.out.println("\n----------------------\n");
+            poczatek();
+            getEnterMyScanner().nextLine();
+        }
+
+        else {
+            miasto();
+
+        }
 
 
+
+
+    }
+
+    public void tawerna() {
+        System.out.println("\n----------------------\n");
+        System.out.println("Jesteś w Tawernie");
+        System.out.println(" ");
+        System.out.println("1:Kup jedzenie (Odnowi HP):");
+        System.out.println("2:Idź spać:");
+        System.out.println("3:Wróć do miasta:");
+        System.out.println("4: Zobacz statystyki :");
+        System.out.println("\n----------------------\n");
+
+        choice = MyScanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("\n----------------------\n");
+            System.out.println("Zjadłeś smaczy posiłek twoje HP jest pełne");
+            System.out.println("\n----------------------\n");
+            graczHP = 100;
+            getEnterMyScanner().nextLine();
+        }
+        if (choice == 2) {
+            System.out.println("\n----------------------\n");
+            System.out.println("Jesteś wypoczęty");
+            System.out.println("\n----------------------\n");
+            getEnterMyScanner().nextLine();
+            tawerna();
+        }
+        if (choice == 3) {
+            miasto();
+        } else if (choice == 4) {
+            System.out.println("\n----------------------\n");
+            System.out.println("Imie: " + graczImie);
+            System.out.println("Klasa: " + graczKlasa);
+            System.out.println("HP: " + graczHP);
+            System.out.println("Pieniądze: " + graczGotowka + "$");
+            System.out.println("Broń: " + graczBron);
+            System.out.println("\n----------------------\n");
+            getEnterMyScanner().nextLine();
+            tawerna();
+
+        } else {
+            tawerna();
+
+        }
+    }
+
+    public void gildia(){
+        System.out.println("\n----------------------\n");
+        System.out.println("Jesteś w Gildi wybierz zadanie");
+        System.out.println(" ");
+        System.out.println("1:Przeszukaj ruiny zamku:");
+        System.out.println("2:--------:");
+        System.out.println("3:Wróć do miasta:");
+        System.out.println("4: Zobacz statystyki :");
+        System.out.println("\n----------------------\n");
+
+        choice = MyScanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("\n----------------------\n");
+            System.out.println("idziesz do zamku");
+            System.out.println("\n----------------------\n");
+            getEnterMyScanner().nextLine();
+            opuszczonyZanek();
+        }
+        if (choice == 3) {
+            miasto();
+        } else if (choice == 4) {
+            System.out.println("\n----------------------\n");
+            System.out.println("Imie: " + graczImie);
+            System.out.println("Klasa: " + graczKlasa);
+            System.out.println("HP: " + graczHP);
+            System.out.println("Pieniądze: " + graczGotowka + "$");
+            System.out.println("Broń: " + graczBron);
+            System.out.println("\n----------------------\n");
+            getEnterMyScanner().nextLine();
+            gildia();
+
+        } else {
+            tawerna();
+
+        }
+
+    }
+
+    public void opuszczonyZanek(){
+        System.out.println("\n----------------------\n");
+        System.out.println("Jesteś w Zamku");
+        System.out.println(" ");
+        System.out.println("1:Walczcz:");
+        System.out.println("2:Uciekaj:");
+        System.out.println("\n----------------------\n");
+
+        choice = MyScanner.nextInt();
+
+//        if (choice == 1) {
+//            fight();
+//        }
+        if (choice == 2) {
+            miasto();
+        }
+
+        else {
+            opuszczonyZanek();
+        }
     }
 }
